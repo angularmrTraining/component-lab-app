5237f741c45aea7946f23be2d7991f128f33f244
Lazhari <lazhari.mohammed@outlook.com>
2018-01-07 16:06:54 +0000

    Set update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { USERS } from './mock-users';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Blog';
+  blogShow: boolean = true;
+  usersListShow: boolean = true;
+  user = {
+    name: 'Mohammed',
+    job: {
+      title: 'full Stack Developer',
+      company: 'V-Line Group'
+    }
+  }
+  usersList = USERS;
+
+  toggleBlog() {
+    this.blogShow = !this.blogShow;
+  }
+  toggleUsers() {
+    this.usersListShow = !this.usersListShow;
+  }
 }
