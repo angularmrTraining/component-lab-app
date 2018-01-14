f1f7200ac09e4d1ba1b0ac240454db19d588e5ce
Lazhari <lazhari.mohammed@outlook.com>
2018-01-14 16:55:07 +0000

    Update components

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { UsersService } from '../../services/users.service';
+import { User } from '../../models/user';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  @Input() users: [Object];
-  constructor() { }
+  @Input() users: User[] = [];
+  constructor(private usersSerivce: UsersService) { }
 
   ngOnInit() {
+    this.usersSerivce.getUsers()
+      .subscribe(resp => {
+        this.users = resp.contacts;
+      })
   }
 
 }
